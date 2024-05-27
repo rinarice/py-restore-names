e6e5b72a7bb29a46e7187bad76f1bf87ad1e6811
Kateryna <kat.rin47u@gmail.com>
2024-05-27 23:40:28 +0100

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,20 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def users() -> list[dict]:
+    return [
+        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+        {"last_name": "Adams", "full_name": "Mike Adams"}
+    ]
+
+
+def test_restore_names(users: list) -> None:
+    restore_names(users)
+
+    expected = [
+        {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
+        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"}
+    ]
+    assert users == expected
